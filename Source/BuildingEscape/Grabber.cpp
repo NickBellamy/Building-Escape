653d3be74205a0f189fb388feface56f8a55ed92
Nick Bellamy <nick.bellamy@gmail.com>
2017-09-07 13:23:32 +0100

    Pseudo code on tick component of Grabber()

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -30,6 +30,10 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player view port this tick
+
+	// Ray-cast out to reach distance
+
+	// See what we hit
 }
 
