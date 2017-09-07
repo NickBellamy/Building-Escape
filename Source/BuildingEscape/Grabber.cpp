9a5eff25fa4ce08e39a1b1f73215c55eb8befb10
Nick Bellamy <nick.bellamy@gmail.com>
2017-09-07 13:26:13 +0100

    Revert "Pseudo code on tick component of Grabber()"
    This reverts commit 653d3be74205a0f189fb388feface56f8a55ed92.

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -30,10 +30,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Get player view port this tick
-
-	// Ray-cast out to reach distance
-
-	// See what we hit
+	// ...
 }
 
