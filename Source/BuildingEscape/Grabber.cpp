71580f59fd3912273340462eccb19a8cd4ea6dda
Nick Bellamy <nick.bellamy@gmail.com>
2017-09-07 14:38:01 +0100

    Log player position to output console every tick

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,9 @@
 #include "BuildingEscape.h"
 #include "Grabber.h"
 
+// Macro to tag input parameters that are changed by the function
+#define OUT
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -31,6 +34,13 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	// Log view point to output console
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *(PlayerViewPointLocation.ToString()), *(PlayerViewPointRotation.ToString()));
 
 	// Ray-cast out to reach distance
 
